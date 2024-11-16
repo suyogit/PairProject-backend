c05f0e399ee5628c384ac2eee6d702c77fb508fc
Suyog Acharya <asuyog042@gmail.com>
2024-11-16 15:50:22 +0545

    playing with routes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,35 @@
 const express=require("express")
 const app=express()
 
-app.use("/test",(req,res)=>{
+// app.use("/test",(req,res)=>{
+//     res.send("Hello test")
+// })
+
+// app.use("/",(req,res)=>{
+//     res.send("Hello World")
+// })
+
+app.get("/user",(req,res)=>{
+    console.log(req.query)
     res.send("Hello test")
 })
 
-app.use("/",(req,res)=>{
-    res.send("Hello World")
+app.get("/cart/:id/:name",(req,res)=>{
+    console.log(req.params)
+    res.send("Hello test")
+})
+
+
+app.get("/test",(req,res)=>{
+    res.send("Hello test")
+})
+
+app.post("/test",(req,res)=>{
+    res.send("posted succesfully")
+})
+
+app.delete("/test",(req,res)=>{
+    res.send("deleted successfully")
 })
 
 app.listen(3000,()=>{
