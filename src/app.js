8a27ef668e7a12416e137e5e9aae3cb7b03b910f
Suyog Acharya <asuyog042@gmail.com>
2024-11-17 22:19:28 +0545

    api level data sanitization

--- src/app.js
+++ src/app.js
@@ -43,11 +43,23 @@ app.delete("/user", async (req, res) => {
 });
 
 //update data of the user by id
-app.patch("/user", async (req, res) => {
-  const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
   const data = req.body;
 
   try {
+    const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed");
+    }
+    if(data?.skills.length>10)
+    {
+        throw new Error("Cannot add more than 10 skills");
+    }
     await User.findByIdAndUpdate(userId, data, {
       returnDocument: "after",
       runValidators: true,
